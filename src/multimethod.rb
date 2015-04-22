6ed5fe013358a4a40788301f0b6ac89d063eb600
lspir <leandrospir@gmail.com>
2015-04-22 18:47:24 -0300

    Diccionario multimethod

--- src/multimethod.rb
+++ src/multimethod.rb
@@ -1,19 +1,26 @@
-class Multimethod
-  attr_accessor :firmas
-  def self.init
-    @firmas = Array.new()
+class MultiMethods
+  attr_accessor :metodos
+
+  def initialize
+    @metodos=Hash.new
   end
 
-  def partial_def firma, clases, &bloque
-    partialBlock = Partial_block.new(clases,&bloque)
-    if (@firmas.select{|firmaElement| firmaElement === firma}.size > 0){
 
-    } else {
-
-    }
-
+  def
+  partial_def firma, clases,&bloque
+    partialBlock = Partial_block_dummy.new(clases,&bloque)
+    if !@metodos.include? firma
+      @metodos.store(firma,[partialBlock])
+    else
+      self.borrarSiEsNecesario(firma,clases)
+      @metodos[firma].push(partialBlock)
+    end
 
   end
 
+
+  def borrarSiEsNecesario(firma,clases)
+    @metodos.store(firma,(@metodos[firma].select{|pB|pB.clases!=clases}))
   end
+
 end
