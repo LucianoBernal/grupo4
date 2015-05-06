20ae5b0b97b3f93876e08284e7945c1f5690f7fa
lspir <leandrospir@gmail.com>
2015-05-06 16:43:32 -0300

    Lucho sacando buscar menor distancia de object

--- src/multimethod.rb
+++ src/multimethod.rb
@@ -6,38 +6,38 @@ class Module
   alias_method :old_respond, :respond_to?
 
   def metodos
-     @metodos = @metodos || Hash.new()
+    @metodos = @metodos || Hash.new()
   end
 
   def metodos_totales
-   ## metodos.merge!(super) {|key, valor_mio, valor_super|
-   ##                valor_mio.concat(valor_super.select{|pB| valor_mio.all?{|pB_prioridad| pB_prioridad.clases!=pB.clases} })}
+    ## metodos.merge!(super) {|key, valor_mio, valor_super|
+    ##                valor_mio.concat(valor_super.select{|pB| valor_mio.all?{|pB_prioridad| pB_prioridad.clases!=pB.clases} })}
     acum=metodos
-    self.ancestors.each{|ancestro| acum= acum.merge!(ancestro.metodos) {|key, valor_mio, valor_super|
-                  valor_mio.concat(valor_super.select{|pB| valor_mio.all?{|pB_prioridad| pB_prioridad.clases!=pB.clases} })}}
+    self.ancestors.each { |ancestro| acum= acum.merge!(ancestro.metodos) { |key, valor_mio, valor_super|
+      valor_mio.concat(valor_super.select { |pB| valor_mio.all? { |pB_prioridad| pB_prioridad.clases!=pB.clases } }) } }
     acum
   end
 
 
-
   def
-  partial_def firma, clases,&bloque
-    partial_block = PartialBlock.new(clases,&bloque)
+  partial_def firma, clases, &bloque
+    partial_block = PartialBlock.new(clases, &bloque)
+    buscar_metodo_menor_distancia= Proc.new { |sym, *argt| self.metodos_totales[sym].select { |pB| pB.matches(*argt) }.min { |left, right| left.distancia(*argt) <=> right.distancia(*argt) } }
     if !metodos.include? firma
-      define_method(firma) {|*args| instance_exec(firma, *args) {|firma, *argt| pB = buscar_metodo_menor_distancia(firma, *argt)
-                                                                                         if pB.nil?
-                                                                                           raise ArgumentError, 'Error de Argumentos'
-                                                                                         end
-                                                                                             pB.call(*argt)}}
+      define_method(firma) { |*args| pB = instance_exec(firma, *args) {buscar_metodo_menor_distancia.call(firma,*args)}
+      if pB.nil?
+        raise ArgumentError, 'Error de Argumentos'
+      end
+      pB.call(*args) }
       metodos.store(firma, [partial_block])
     else
-      self.borrarSiEsNecesario(firma,clases)
+      self.borrarSiEsNecesario(firma, clases)
       metodos[firma].push(partial_block)
     end
   end
 
-  def borrarSiEsNecesario(firma,clases)
-    @metodos.store(firma,(@metodos[firma].select{|pB|pB.clases!=clases}))
+  def borrarSiEsNecesario(firma, clases)
+    @metodos.store(firma, (@metodos[firma].select { |pB| pB.clases!=clases }))
   end
 
   def multimethods
@@ -54,34 +54,26 @@ class Module
       self.old_respond(sym, priv) || self.multimethods.include?(sym)
     else
       clases_obtenidas = metodos_totales[sym] || Array.new
-      clases_obtenidas.any? {|pB| pB.clases = clases}
+      clases_obtenidas.any? { |pB| pB.clases = clases }
     end
 
   end
 end
 
 class Object
-  def buscar_metodo_menor_distancia firma, *args
-    #@metodos[firma].min {|pB| } Seria algo asi..
-    #Esto no esta terminado, por lo pronto devuelve el ultimo pb agregado
-    self.class.metodos_totales[firma].select{|pB| pB.matches(*args)}.min {|left, right| left.distancia(*args) <=> right.distancia(*args)}
-    #Si el resultado de min puede ser en algunos casos un array deberiamos agarrar el primero
-    #self.class.metodos[firma][self.class.metodos[firma].size - 1]
-  end
-
   def
-    partial_def firma,clases,&bloque
-    self.singleton_class.partial_def firma,clases,&bloque
+  partial_def firma, clases, &bloque
+    self.singleton_class.partial_def firma, clases, &bloque
   end
 
 end
 
 class A
-  partial_def :concat, [String, String] do |s1,s2|
+  partial_def :concat, [String, String] do |s1, s2|
     s1 + s2
   end
 
-  partial_def :concat, [String, Integer] do |s1,n|
+  partial_def :concat, [String, Integer] do |s1, n|
     s1 * n
   end
 
