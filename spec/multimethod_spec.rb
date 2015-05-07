820124965e0348b552c50ab5fc112e5983b6799b
Luciano Bernal <bernal.luciano@gmail.com>
2015-05-07 16:32:23 -0300

    tests

--- spec/multimethod_spec.rb
+++ spec/multimethod_spec.rb
@@ -18,6 +18,33 @@ describe 'Multimethod tests' do
   end
   a = A.new
 
+  class Tanque
+
+    def atacar_con_metralla objetivo
+      "Tra tra tra"
+    end
+    def atacar_con_canion objetivo
+      "canionazooo"
+    end
+    def atacar_con_satelite objetivo
+      "satelite"
+    end
+    def atacar_distinto objetivo
+      "distinto"
+    end
+  end
+
+  class Soldado
+    attr_accessor :nombre
+
+    def initialize nombre
+      @nombre = nombre
+    end
+  end
+
+  class Avion
+  end
+
   it 'concat("hello", " world") devuelve "helloworld"' do
     expect(a.concat('hello', ' world')).to eq('hello world')
   end
@@ -46,18 +73,58 @@ describe 'Multimethod tests' do
     expect(a.concat(Object.new,3)).to eq("Objetos concatenados")
   end
 
-  it 'agrega multimethods a un unico objeto' do
+  it 'multimethod permite soporta self' do
     class Tanque
+      partial_def :ataca_a, [Tanque] do |objetivo|
+        self.atacar_con_canion(objetivo)
+      end
+      partial_def :ataca_a, [Soldado] do |objetivo|
+        self.atacar_con_metralla(objetivo)
+      end
     end
-    class Soldado
-      attr_accessor :nombre
+    t = Tanque.new
+    expect(t.ataca_a(Soldado.new("Ryan"))).to eq("Tra tra tra")
+    expect(t.ataca_a(Tanque.new)).to eq("canionazooo")
+  end
 
-      def initialize nombre
-        @nombre = nombre
+  it 'definicion parcial nueva se agrega a anteriores y no las pisa' do
+    class Tanque
+      partial_def :ataca_a, [Tanque] do |objetivo|
+        self.atacar_con_canion(objetivo)
+      end
+      partial_def :ataca_a, [Soldado] do |objetivo|
+        self.atacar_con_metralla(objetivo)
+      end
+     partial_def :ataca_a, [Avion] do |avion|
+       self.atacar_con_satelite(avion)
+     end
+    end
+    expect(Tanque.new.ataca_a(Avion.new)).to eq("satelite")
+    expect(Tanque.new.ataca_a(Soldado.new("Ryan"))).to eq("Tra tra tra")
+    expect(Tanque.new.ataca_a(Tanque.new)).to eq("canionazooo")
+  end #MAL PROGRAMADO
+
+  it 'definicion parcial cambiada pisa la anterior' do
+    class Tanque
+      partial_def :ataca_a, [Tanque] do |objetivo|
+        self.atacar_con_canion(objetivo)
+      end
+      partial_def :ataca_a, [Soldado] do |objetivo|
+        self.atacar_con_metralla(objetivo)
+      end
+      partial_def :ataca_a, [Avion] do |avion|
+        self.atacar_con_satelite(avion)
       end
     end
     class Tanque
+      partial_def :ataca_a, [Soldado] do |soldado|
+        self.atacar_distinto(soldado)
+      end
     end
+    expect(Tanque.new.ataca_a(Soldado.new("Ryan"))).to eq("distinto")
+  end
+
+  it 'agrega multimethods a un unico objeto' do
     tanque_modificado = Tanque.new
     tanque_modificado.partial_def :bocinar, [Soldado] do |soldado|
       "Honk Honk! #{soldado.nombre}"
@@ -65,7 +132,6 @@ describe 'Multimethod tests' do
     tanque_modificado.partial_def :bocinar, [Tanque] do |tanque|
       "Hooooooonk!"
     end
-
     expect(tanque_modificado.bocinar(Soldado.new("pepe"))).to eq("Honk Honk! pepe")
     expect(tanque_modificado.bocinar(Tanque.new)).to eq("Hooooooonk!")
     expect{Tanque.new.tocar_bocina(Tanque.new)}.to raise_error(NoMethodError)
