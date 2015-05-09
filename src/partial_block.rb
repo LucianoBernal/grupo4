392427045fb9bfde13b1ead3aad622526b1425cf
Pato94 <pato94_06@hotmail.com>
2015-05-09 10:50:23 -0300

    Sutiles cambios en partialBlock

--- src/partial_block.rb
+++ src/partial_block.rb
@@ -14,7 +14,12 @@ class PartialBlock
   end
 
   def argumentosCorrectos argumentos
-    argumentos.all? { |argumento| argumento.is_a? @clases[argumentos.index(argumento)] }
+
+    argumentos.map.with_index { |argumento, index| argumento.is_a? @clases[index]}.all?
+    #argumentos.all? { |argumento| argumento.is_a? @clases[argumentos.index(argumento)] }
+    #de esta forma (la comentada)un partialBlock con [String, Integer] matchea con ("hola", "hola")
+    #porque al evaluar el segundo argumento la expresion -argumentos.index(argumento)-
+    #retorno 0, y clases[0] es String.
   end
 
   def longitudCorrecta(argumentos)
@@ -27,8 +32,10 @@ class PartialBlock
   end
 
   def distancia(*args)
-    retorno = 0
-    args.each_with_index {|elem, index| retorno += elem.class.ancestors.index(@clases[index])*(index+1)}
-    retorno
+    #retorno = 0
+    #args.each_with_index {|elem, index| retorno += elem.class.ancestors.index(@clases[index])*(index+1)}
+    #retorno
+    #Sori pero si no es en una linea yo no lo hago (?)
+    (args.map.with_index {|argumento, index| argumento.class.ancestors.index(@clases[index])*(index+1)}).reduce :+
   end
 end
